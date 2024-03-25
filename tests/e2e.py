6c731ea0dbb8f004e014182faf28435b0e57f696
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-03-25 14:21:58 +0000

    refactor test

--- tests/e2e.py
+++ tests/e2e.py
@@ -15,6 +15,21 @@ def text_area():
         area.type = original_type
 
 
+@contextlib.contextmanager
+def new_text(area=bpy.types.Area):
+    space = area.spaces[0]  # active space
+    if not isinstance(space, bpy.types.SpaceTextEditor):
+        raise Exception("unnexpected space type")
+
+    bpy.ops.text.new()
+    text = space.text
+
+    try:
+        yield text
+    finally:
+        bpy.ops.text.unlink()
+
+
 def main():
     # prepare
     addon_utils.enable("myaddon")
@@ -23,18 +38,14 @@ def main():
     # execute operator
     input_str = "import bpy \n"
     expected_str = "import bpy\n"
-    with text_area() as area:
-        with bpy.context.temp_override(area=area):
-            space = area.spaces[0]  # active space
-            if not isinstance(space, bpy.types.SpaceTextEditor):
-                raise Exception("unnexpected space type")
-
-            bpy.ops.text.new()
-            text = space.text
-            text.from_string(input_str)
-            bpy.ops.black_blender.format()
-            actual_str = text.as_string()  # None
-            bpy.ops.text.unlink()
+    with (
+        text_area() as area,
+        bpy.context.temp_override(area=area),
+        new_text(area) as text,
+    ):
+        text.from_string(input_str)
+        bpy.ops.black_blender.format()
+        actual_str = text.as_string()
 
     # assertion
     if actual_str != expected_str:
